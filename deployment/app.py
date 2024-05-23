d394c79cbbbd14cd6237458b88f0f621605fb265
Tea <111054943+yowaiii@users.noreply.github.com>
2024-05-23 02:01:26 -0700

    Add files via upload

---
+++ deployment/app.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pickle
+from flask import Flask,request,render_template
+
+
+app=Flask(__name__)
+
+model=pickle.load(open('models/stdgrade.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features=[float(x) for x in request.form.values()]
+    features=[np.array(int_features)]
+    prediction=model.predict(features)
+    
+    output=(prediction)
+    
+    return render_template('index.html',prediction_text='Predicted Grade is: {}'.format(output))
+
+if __name__ == '__main__':
+    app.run()
